654e1d35014cb122f2c9d26d25257d336dbce7a0
adnanjpg <adnanfahedhr@gmail.com>
2023-10-24 00:54:15 +0300

    feat: init totp qr generation and generating matching code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn req_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+mod get_otp_qr;
+mod get_totp;
 
+async fn req_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     match req.uri().path() {
         "/hello" => {
             let response = Response::builder()
@@ -13,6 +15,38 @@ async fn req_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
                 .header("Content-Type", "text/plain")
                 .body(Body::from("Hello World\r\n"))
                 .unwrap();
+            Ok(response)
+        }
+        "/get-otp-qr" => {
+            let qr_code = get_otp_qr::generate_otp_qr_code();
+
+            get_otp_qr::outputqr(&qr_code.to_string()).unwrap();
+
+            let response = Response::builder()
+                .status(200)
+                .header("Content-Type", "text/plain")
+                .body(Body::from(""))
+                .unwrap();
+
+            Ok(response)
+        }
+        "/get-current-totp" => {
+            // The TOTP time step is typically 30 seconds.
+            let time_step = 30;
+
+            // TODO(adnanjpg): get from saved secret
+            let randomk = "F23FZGOU3CW4AYDQYXAGUNUYKLCHKVYB";
+
+            let totp = get_totp::generate_totp(randomk.to_owned(), time_step);
+
+            println!("totp iss {}", totp);
+
+            let response = Response::builder()
+                .status(200)
+                .header("Content-Type", "text/plain")
+                .body(Body::from(""))
+                .unwrap();
+
             Ok(response)
         }
         _ => {
@@ -28,13 +62,8 @@ async fn req_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 
 #[tokio::main]
 async fn main() {
-
-    let server = Server::bind(&SocketAddr::from(([127,0,0,1],8080))).serve(
-        make_service_fn(|_conn| {
-            async {
-                Ok::<_, Infallible>(service_fn(req_handler))
-            }
-        }
+    let server = Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8080))).serve(make_service_fn(
+        |_conn| async { Ok::<_, Infallible>(service_fn(req_handler)) },
     ));
 
     if let Err(e) = server.await {
