b4e9a8f26d69c1ae3ec967a6f93d10e737cedad0
adnanjpg <adnanfahedhr@gmail.com>
2023-10-31 13:55:11 +0300

    feat: get fcm project id from service key file

--- src/notification_service.rs
+++ src/notification_service.rs
@@ -6,11 +6,46 @@ mod jwt;
 
 use fb_access_token::ServiceAccount;
 
-// TODO(adnanjpg): take from the file that path is at
-// `GOOGLE_APPLICATION_CREDENTIALS` env variable
-const PROJECT_ID: &str = "remon-mobile-b0c23";
-
 const SERVICE_KEY_FILE_NAME: &str = ".remon-mobile-fcm-creds.json";
+
+fn read_service_key_file() -> Result<String, String> {
+    let key_path = SERVICE_KEY_FILE_NAME;
+
+    let private_key_content = match std::fs::read(key_path) {
+        Ok(content) => content,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    Ok(String::from_utf8(private_key_content).unwrap())
+}
+
+fn read_service_key_file_json() -> Result<serde_json::Value, String> {
+    let file_content = match read_service_key_file() {
+        Ok(content) => content,
+        Err(err) => return Err(err),
+    };
+
+    let json_content: serde_json::Value = match serde_json::from_str(&file_content) {
+        Ok(json) => json,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    Ok(json_content)
+}
+
+fn get_project_id() -> Result<String, String> {
+    let json_content = match read_service_key_file_json() {
+        Ok(json) => json,
+        Err(err) => return Err(err),
+    };
+
+    let project_id = match json_content["project_id"].as_str() {
+        Some(project_id) => project_id,
+        None => return Err("could not get project_id".to_string()),
+    };
+
+    Ok(project_id.to_string())
+}
 
 pub async fn access_token() -> Result<String, ()> {
     let scopes = vec!["https://www.googleapis.com/auth/firebase.messaging"];
@@ -32,14 +67,15 @@ pub async fn send_notification_to(device_id: &str) -> Result<bool, String> {
 }
 
 pub async fn send_notification_to_multi(device_ids: &Vec<&str>) -> Result<bool, String> {
-    // using reqwest, send a request to fcm v1, with the
-    // title "test title", and the message "test message"
+    let project_id = match get_project_id() {
+        Ok(project_id) => project_id,
+        Err(err) => return Err(err),
+    };
 
     // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send
-
     let url = format!(
         "https://fcm.googleapis.com/v1/projects/{}/messages:send",
-        PROJECT_ID
+        project_id
     );
 
     let body = json!({
