b3584f4a98bb247391bcbc769a6f31aaa57f07dd
isaidsari <ismailsaid.sari@stu.fsm.edu.tr>
2024-03-08 13:59:53 +0300

    add senidng notification via grpc request

--- src/grpc/grpc_service.rs
+++ src/grpc/grpc_service.rs
@@ -1,5 +1,17 @@
-use log::{info};
+use log::{error, info};
 use tonic::{transport::Server, Request, Response, Status};
+
+use remonproto::{
+    notification_service_server::{
+        NotificationService as NotificationServiceImpl, NotificationServiceServer,
+    },
+    NotificationRequest, NotificationResponse,
+};
+
+use crate::notification_service;
+use crate::persistence::notification_logs::NotificationType;
+
+use crate::monitor::persistence::fetch_monitor_configs;
 
 pub mod remonproto {
     tonic::include_proto!("remonproto");
@@ -7,11 +19,6 @@ pub mod remonproto {
     pub(super) const FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("remonproto_descriptor");
 }
-
-use remonproto::{
-    notification_service_server::{NotificationService as NotificationServiceImpl, NotificationServiceServer},
-    NotificationRequest, NotificationResponse,
-};
 
 #[derive(Default)]
 pub struct NotificationService;
@@ -29,10 +36,32 @@ impl NotificationServiceImpl for NotificationService {
             request.title, request.body
         );
 
-        // TODO(isaidsari): send notification here
+        let configs = fetch_monitor_configs().await.unwrap_or_else(|e| {
+            error!("failed to fetch monitor configs: {}", e);
+            vec![]
+        });
 
-        let response = remonproto::NotificationResponse {
-            message: "Notification received".into(),
+        let res = notification_service::send_notification_to_single(
+            &configs[0].device_id,
+            &configs[0].fcm_token,
+            &notification_service::NotificationMessage {
+                title: request.title,
+                body: request.body,
+            },
+            &NotificationType::StatusLimitsExceeding,
+        )
+        .await;
+
+        info!("Notification sent: {:?}", res);
+
+        let response = if res.is_ok() {
+            NotificationResponse {
+                message: "Notification sent successfully".to_string(),
+            }
+        } else {
+            NotificationResponse {
+                message: format!("Failed to send notification: {}", res.err().unwrap()),
+            }
         };
 
         // return the response
@@ -52,11 +81,14 @@ pub async fn init() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("gRPC service listening on {}", addr);
 
-    Server::builder()
-        .add_service(NotificationServiceServer::new(notification_service))
-        .add_service(reflection_service)
-        .serve(addr)
-        .await?;
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(NotificationServiceServer::new(notification_service))
+            .add_service(reflection_service)
+            .serve(addr)
+            .await
+            .unwrap();
+    });
 
     Ok(())
 }
